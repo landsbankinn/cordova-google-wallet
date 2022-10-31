71b5f49386b8cffb86e31df1c5a576699953b3d8
Vignir Ómar Vignisson Löve <vignir.o.love@landsbankinn.is>
2022-10-31 10:58:34 +0000

    Add all types to index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -48,6 +48,47 @@ export function GetStableHardwareId(): Promise<
     }
 >;
 
+export type GoogleWalletTokenInfo = {
+  /**
+   * The token reference ID.
+   */
+  issuerTokenId: string;
+  /**
+   * The name of the issuer.
+   */
+  issuerName: string;
+  /**
+   * The last four digits of the FPAN.
+   */
+  fpanLastFour: string;
+  /**
+   * The last four digits of the DPAN.
+   */
+  dpanLastFour: string;
+  /**
+   * The token provider specifies the tokenization service used to
+   * create a given token. The terms “token provider” and
+   * “token service provider” (TSP) are used interchangeably in this API.
+   */
+  tokenServiceProvider: GoogleWalletTokenProvider;
+  /**
+   * The CardNetwork.
+   */
+  network: GoogleWalletCardNetwork;
+  /**
+   * The TokenState.
+   */
+  tokenState: GoogleWalletTokenState;
+  /**
+   * True if the token is set as the default.
+   */
+  isDefaultToken: boolean;
+  /**
+   * The card portfolio.
+   */
+  portfolioName: string;
+};
+
 /**
  * List tokens in the active wallet
  *
@@ -70,21 +111,7 @@ export function listTokens(): Promise<
     }
 >;
 
-/**
- * Client-side push provisioning
- *
- * pushTokenize starts the push tokenization flow in which the issuer provides most or all
- * card details needed for Google Pay to get a valid token. Tokens added using this method
- * are added to the active wallet.
- *
- * https://developers.google.com/pay/issuers/apis/push-provisioning/android/wallet-operations?authuser=2#client-side_push_provisioning
- */
-export function pushTokenize({
-  opc,
-  displayName,
-  lastDigits,
-  address,
-}: {
+export type PushTokenizeArgs = {
   /**
    * Opaque Payment Card JWT
    */
@@ -123,7 +150,23 @@ export function pushTokenize({
     /** The postal or zip code */
     postalCode?: string;
   };
-}): Promise<
+};
+
+/**
+ * Client-side push provisioning
+ *
+ * pushTokenize starts the push tokenization flow in which the issuer provides most or all
+ * card details needed for Google Pay to get a valid token. Tokens added using this method
+ * are added to the active wallet.
+ *
+ * https://developers.google.com/pay/issuers/apis/push-provisioning/android/wallet-operations?authuser=2#client-side_push_provisioning
+ */
+export function pushTokenize({
+  opc,
+  displayName,
+  lastDigits,
+  address,
+}: PushTokenizeArgs): Promise<
   | {
       type: "result";
       tokenId: string;
@@ -151,3 +194,109 @@ export function isCardInWallet({ identifier }: { identifier: string }): Promise<
       message: string;
     }
 >;
+
+/* ENUMS */
+
+export enum GoogleWalletStatusCodes {
+  UNKNOWN_ERROR = 0,
+  /**
+   * There is no active wallet.
+   */
+  NO_ACTIVE_WALLET = 15002,
+  /**
+   * The indicated issuer token ID does not match a token in the active wallet.
+   * This status can be returned by calls that specify an issuer token ID.
+   */
+  TOKEN_NOT_FOUND = 15003,
+  /**
+   * The specified token was found but was not in a legal state for the
+   * operation to succeed. For example, this can happen when attempting to
+   * select as default a token that is not in the state TOKEN_STATE_ACTIVE.
+   */
+  INVALID_TOKEN_STATE = 15004,
+  /**
+   * Tokenization failed because the device did not pass a compatibility check.
+   */
+  ATATTESTATION_ERROR = 15005,
+  /**
+   * The TapAndPay API cannot be called by the current application.
+   * If you get this error, make sure you are calling the API using a package name
+   * and fingerprint that we have added to our allowlist.
+   *
+   * The app needs to be verified first by Google to be able to talk to the
+   * TapAndPay api. See readme for further information.
+   */
+  UNAVAILABLE = 15009,
+
+  // No documentation was found for following enum values
+
+  SAVE_CARD_ERROR = 15019,
+  INELIGIBLE_FOR_TOKENIZATION = 15021,
+  TOKENIZATION_DECLINED = 15022,
+  CHECK_ELIGIBILITY_ERROR = 15023,
+  TOKENIZE_ERROR = 15024,
+  TOKEN_ACTIVATION_REQUIRED = 15025,
+  PAYMENT_CREDENTIALS_DELIVERY_TIMEOUT = 15026,
+  USER_CANCELED_FLOW = 15027,
+  ENROLL_FOR_VIRTUAL_CARDS_FAILED = 15028,
+}
+
+export enum GoogleWalletCardNetwork {
+  AMEX = 1,
+  DISCOVER = 2,
+  MASTERCARD = 3,
+  VISA = 4,
+  INTERAC = 5,
+  PRIVATE_LABEL = 6,
+  EFTPOS = 7,
+  MAESTRO = 8,
+  ID = 9,
+  QUICPAY = 10,
+  JCB = 11,
+  ELO = 12,
+  MIR = 13,
+}
+
+export enum GoogleWalletTokenProvider {
+  AMEX = 2,
+  MASTERCARD = 3,
+  VISA = 4,
+  DISCOVER = 5,
+  EFTPOS = 6,
+  INTERAC = 7,
+  OBERTHUR = 8,
+  PAYPAL = 9,
+  JCB = 13,
+  ELO = 14,
+  GEMALTO = 15,
+  MIR = 16,
+}
+export enum GoogleWalletTokenState {
+  /**
+   * This state is visible in the SDK but is not possible for push
+   * provisioning. You can safely ignore this state.
+   */
+  UNTOKENIZED = 1,
+  /**
+   * The token is not currently available for payments,
+   * but will be after some time.
+   */
+  PENDING = 2,
+  /**
+   * The token is in the active wallet, but requires additional user
+   * authentication for use (yellow path step-up).
+   */
+  NEEDS_IDENTITY_VERIFICATION = 3,
+  /**
+   * The token has been temporarily suspended.
+   */
+  SUSPENDED = 4,
+  /**
+   * The token is active and available for payments.
+   */
+  ACTIVE = 5,
+  /**
+   * Token has been issued by TSP but Felica provisioning has not been completed.
+   */
+  FELICA_PENDING_PROVISIONING = 6,
+}
